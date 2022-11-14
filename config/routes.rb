a37ee3bc2c14318a0c2ba7b4f6548777becad0a8
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-14 13:47:06 -0700

    delete favorite functionality working

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :learning_resources, only: :index
       resources :users, only: :create
       resources :favorites, only: [:create, :index]
+      resource :favorites, only: :destroy
       resources :tourist_sights, only: :index
     end
   end
