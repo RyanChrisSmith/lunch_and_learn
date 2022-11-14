35d0d81b82139d45ee53aa0c18634da1ac277e10
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-14 08:27:30 -0700

    mispelling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:name, :email, :password, :password_confrimation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 
 end
