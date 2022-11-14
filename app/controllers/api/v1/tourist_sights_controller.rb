fdd7b2698c1b6944e341d1a0b9b0921282d44745
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-14 09:19:11 -0700

    first test added and passing, time to build the api call

---
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -0,0 +1,4 @@
+class Api::V1::TouristSightsController < ApplicationController
+  def index
+  end
+end
---
+++ spec/requests/api/v1/tourist_sights_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'tourist sights API' do 
+  describe 'happy path' do 
+    it 'will return all tourists sights within a 20,000 meter radius of the capital city' do 
+      get '/api/v1/tourist_sights?country=France'
+
+      expect(response).to be_successful
+    end
+  end 
+end
