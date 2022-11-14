17105a12e30ac93b0243aa48b75dedb963c41f26
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-14 11:23:37 -0700

    still working on stubbing

--- spec/requests/api/v1/tourist_sights_spec.rb
+++ spec/requests/api/v1/tourist_sights_spec.rb
@@ -28,13 +28,11 @@ RSpec.describe 'tourist sights API' do
     end
 
     it "will return a random country's tourists sights within a 20,000 meter radius of the capital city if no country param entered", :vcr do
-      sights = TouristSightsFacade.random
-      allow_any_instance_of(Api::V1::TouristSightsController).to receive(:random).and_return(sights)
 
       get '/api/v1/tourist_sights?country='
       expect(response).to be_successful
       country = JSON.parse(response.body, symbolize_names: true)
-
+      require 'pry' ; binding.pry
     end
   end
 end
